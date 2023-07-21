dcfbebeecda8c75802999fc19b08878241fa4013
Kevin Henrique <kevinhleonardi@gmail.com>
2023-07-21 13:04:35 -0300

    Mais Ajustes

--- cafe/script.js
+++ cafe/script.js
@@ -1,24 +1,23 @@
 // Array de endereços de exemplo
 const addresses = [
-    "Rua A, 123",
-    "Avenida B, 456",
-    "Praça C, 789",
-    "Alameda D, 101",
-    "Estrada E, 222",
-    "Futuramente adicionar campo próprio do google maps contendo os endereços"
-  ];
-  
-  // Função para criar a lista de endereços
-  function createAddressList() {
-    const addressList = document.getElementById("address-list");
+  "Rua A, 123",
+  "Avenida B, 456",
+  "Praça C, 789",
+  "Alameda D, 101",
+  "Estrada E, 222",
+  "Futuramente adicionar extensão com google maps",
+];
 
-    addresses.forEach((address) => {
-      const listItem = document.createElement("div");
-      listItem.textContent = address;
-      addressList.appendChild(listItem);
-    });
-  }
-  
-  // Chama a função para criar a lista de endereços quando a página é carregada
-  window.addEventListener("load", createAddressList);
-  
+// Função para criar a lista de endereços
+function createAddressList() {
+  const addressList = document.getElementById("address-list");
+
+  addresses.forEach((address) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = address;
+    addressList.appendChild(listItem);
+  });
+}
+
+// Chama a função para criar a lista de endereços quando a página é carregada
+window.addEventListener("load", createAddressList);
